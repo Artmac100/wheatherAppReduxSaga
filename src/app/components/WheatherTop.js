13dc51845e9aa55f2c7de3cfe27b8effe436b444
Artem <artmakatera@gmail.com>
2017-09-19 11:12:39 +0300

    convert wheatherTop to function

--- src/app/components/WheatherTop.js
+++ src/app/components/WheatherTop.js
@@ -5,45 +5,42 @@ import { toHgmm } from '../utils/converter';
 import { toDatestring, toTimeSting } from '../utils/dateConverter';
 import { wheatherIcon } from '../utils/whetherTimeUtil';
 
-class WheatherTop extends React.Component {
-
-  render() {
-    const { weather, dt_txt, main, wind } = this.props.wheather;
-    return (
-      <div className="wheather-contatiner">
-        <div className="datetime">
-          { toDatestring(dt_txt) } { toTimeSting(dt_txt) }
-        </div>
-        <div className="wheather-props">
-          <div className="main-wheather-prop">
-            <div className="cloudness">
-              <div className="cloudness-icon"><i className={wheatherIcon(weather[0].id, dt_txt)} /></div>
-              <div className="cloudness-text">{weather[0].description}</div>
-            </div>
-            <div className="temprature">{main.temp.toFixed()}<span className="temprature-unit">°C</span></div>
+const WheatherTop = (props) => {
+  const { weather, dt_txt, main, wind } = props.wheather;
+  return (
+    <div className="wheather-contatiner">
+      <div className="datetime">
+        { toDatestring(dt_txt) } { toTimeSting(dt_txt) }
+      </div>
+      <div className="wheather-props">
+        <div className="main-wheather-prop">
+          <div className="cloudness">
+            <div className="cloudness-icon"><i className={wheatherIcon(weather[0].id, dt_txt)} /></div>
+            <div className="cloudness-text">{weather[0].description}</div>
           </div>
-          <div className="extra-wheather-props">
-            <div className="wheather-prop">
-              <span className="wheather-prop-icon"><i className="icon-wind" /></span>
-              {wind.speed}
-              <span className="wheather-prop-unit"> m/s</span>
-            </div>
-            <div className="wheather-prop">
-              <span className="wheather-prop-icon"><i className="fa fa-compass" aria-hidden="true" /></span>
-              {toHgmm(main.pressure)}
-              <span className="wheather-prop-unit"> mm. mercury</span>
-            </div>
-            <div className="wheather-prop">
-              <span className="wheather-prop-icon"><i className="fa fa-tint" aria-hidden="true" /></span>
-              {main.humidity}
-              <span className="wheather-prop-unit"> %</span>
-            </div>
+          <div className="temprature">{main.temp.toFixed()}<span className="temprature-unit">°C</span></div>
+        </div>
+        <div className="extra-wheather-props">
+          <div className="wheather-prop">
+            <span className="wheather-prop-icon"><i className="icon-wind" /></span>
+            {wind.speed}
+            <span className="wheather-prop-unit"> m/s</span>
+          </div>
+          <div className="wheather-prop">
+            <span className="wheather-prop-icon"><i className="fa fa-compass" aria-hidden="true" /></span>
+            {toHgmm(main.pressure)}
+            <span className="wheather-prop-unit"> mm. mercury</span>
+          </div>
+          <div className="wheather-prop">
+            <span className="wheather-prop-icon"><i className="fa fa-tint" aria-hidden="true" /></span>
+            {main.humidity}
+            <span className="wheather-prop-unit"> %</span>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 WheatherTop.propTypes = {
   wheather: PropTypes.shape({
